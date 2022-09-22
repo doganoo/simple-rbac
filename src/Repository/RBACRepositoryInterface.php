1d1f290a4826c947e8e7ce63c57154c9dccd7395
Doğan Uçar <dogan@dogan-ucar.de>
2022-09-22 13:43:34 +0200

    add new method

--- src/Repository/RBACRepositoryInterface.php
+++ src/Repository/RBACRepositoryInterface.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace doganoo\SimpleRBAC\Repository;
 
+use doganoo\PHPAlgorithms\Datastructure\Lists\ArrayList\ArrayList;
 use doganoo\PHPAlgorithms\Datastructure\Table\HashTable;
 use doganoo\SimpleRBAC\Entity\UserInterface;
 
@@ -11,5 +12,7 @@ interface RBACRepositoryInterface {
     public function getRolesByUser(UserInterface $user): HashTable;
 
     public function getPermissionsByRoleId(int $roleId): HashTable;
+
+    public function getAllPermissions(): ArrayList;
 
 }
