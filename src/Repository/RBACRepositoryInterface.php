5d26266b5c449607387e9cc714e5db8e17d344e1
Doğan Uçar <dogan@dogan-ucar.de>
2022-09-22 13:17:49 +0200

    update name

--- src/Repository/RBACRepositoryInterface.php
+++ src/Repository/RBACRepositoryInterface.php
@@ -4,13 +4,12 @@ declare(strict_types=1);
 namespace doganoo\SimpleRBAC\Repository;
 
 use doganoo\PHPAlgorithms\Datastructure\Table\HashTable;
-use doganoo\SimpleRBAC\Entity\RoleInterface;
 use doganoo\SimpleRBAC\Entity\UserInterface;
 
 interface RBACRepositoryInterface {
 
     public function getRolesByUser(UserInterface $user): HashTable;
 
-    public function getPermissionsByRole(RoleInterface $role): HashTable;
+    public function getPermissionsByRoleId(int $roleId): HashTable;
 
 }
